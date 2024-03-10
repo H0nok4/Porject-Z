c4dd22ed9e28494c72f6e1781f4d92c4026e6e8d
ChenWei <704017380@qq.com>
2024-03-11 00:33:04 +0800

    assets:行为树准备加个随机节点

---
+++ Assets/Scripts/Gameplay/ThinkSystem/ThinkNode_RandomChoose.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ThinkNode_RandomChoose : ThinkNode
+{
+    public override ThinkResult GetResult(Thing_Unit unit)
+    {
+        if (Children.Count > 0)
+        {
+            int index = UnityEngine.Random.Range(0, Children.Count);
+            return Children[index].GetResult(unit);
+        }
+
+        return ThinkResult.NoJob;
+    }
+}
