a98ca4421b6efd33b854c3f784b2cc9045f45a0a
H0nok4 <704017380@qq.com>
2024-03-18 00:23:17 +0800

    assets:准备做建造工作

---
+++ Assets/Scripts/Gameplay/Things/ThingUnit_WorkSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ThingUnit_WorkSetting
+{
+    public Thing_Unit Unit;
+
+    public List<WorkGiver> UsedWorkGivers = new List<WorkGiver>();
+}
--- Assets/Scripts/Gameplay/ThinkSystem/JobGiver/JobGiver_GetWork.cs
+++ Assets/Scripts/Gameplay/ThinkSystem/JobGiver/JobGiver_GetWork.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Assets.Scripts.Gameplay.ThinkSystem.JobGiver {
-    public class JobGiver_GetWork : ThinkNode {
+    public class JobGiver_GetWork : ThinkNode
+    {
         public override ThinkResult GetResult(Thing_Unit unit)
         {
-            throw new NotImplementedException();
+            var workGiverList = unit.WorkSettings
         }
     }
 }
