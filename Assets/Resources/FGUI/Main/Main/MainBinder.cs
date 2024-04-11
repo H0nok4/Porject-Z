d53e88e59717748fd0bd54fb971b88e036a406ca
chenzhenwei <ie2_0o26osxs1@dingtalk.com>
2024-04-11 19:00:50 +0800

    assets:先做个测试UI

---
+++ Assets/Resources/FGUI/Main/Main/MainBinder.cs
@@ -0,0 +1,14 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace Main
+{
+    public class MainBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(UI_MainView.URL, typeof(UI_MainView));
+        }
+    }
+}
