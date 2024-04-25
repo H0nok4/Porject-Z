0fce32654b991017744a9f05ac716c629e2e66a7
H0nok4 <704017380@qq.com>
2024-04-25 22:48:42 +0800

    assets:解决报错

--- Assets/ThirdParty/Scripts/Custom/GComponentExtension.cs
+++ Assets/ThirdParty/Scripts/Custom/GComponentExtension.cs
@@ -1,13 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FairyGUI;
 
 namespace FairyGUI {
-    public partial class GComponent {
+    public partial class GComponent
+    {
         public List<GObject> _children;
-
     }
 }
