12544811af142274eec8fe7d24f1f071705199d0
H0nok4 <704017380@qq.com>
2024-04-29 00:43:29 +0800

    assets:准备做战争迷雾

--- Assets/Scripts/Gameplay/FogOfWar/FogManager.cs
+++ Assets/Scripts/Gameplay/FogOfWar/FogManager.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 
 public class FogManager : Singleton<FogManager>
 {
-
+    private int _width;
+    private int _height;
     public RenderTexture[] FogTexture;
-    public bool[][] VisibleFog;
+    public Color32[][] FogColors;
 
-    public void Init(int mapLayerCount)
+    public void Init(int mapLayerCount,int width,int height)
     {
+        _width = width;
+        _height = height;
+        //后面会有多层地图,需要多层迷雾
+        FogTexture = new RenderTexture[mapLayerCount];
+        FogColors = new Color32[mapLayerCount][];
+        for (int i = 0; i < mapLayerCount ;i++)
+        {
+            FogTexture[i] = RenderTexture.GetTemporary(width, height, 0);
+            FogColors[i] = new Color32[width * height];
+        }
+    }
 
+    public int ToColorIndex(int x, int y)
+    {
+        return x + y * _height;
     }
 }
 
