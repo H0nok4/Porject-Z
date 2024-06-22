d2420b9ebfa32b6fcf6425c4eaacdadea161d138
H0nok4 <704017380@qq.com>
2024-06-23 02:58:17 +0800

    assets:jobdriver相关

---
+++ Assets/Scripts/Gameplay/JobSystem/Driver/JobDriver_GetFoodAndEat.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConfigType;
+
+public class JobDriver_GetFoodAndEat : JobDriver{
+    public override IEnumerable<Work> MakeWorks() {
+        //TODO:移动到目标位置,捡起来,然后原地吃
+        var reserveWork = WorkUtility_Reserve.ReserveTarget(JobTargetIndex.A);
+        yield return reserveWork;
+        yield return WorkUtility_MoveTo.MoveToThing(JobTargetIndex.A, PathMoveEndType.Touch);
+        yield return WorkUtility_Haul.StartCarryThing(JobTargetIndex.A);
+        //TODO:吃
+        yield return WorkUtility_Food.StartEad(JobTargetIndex.A);
+
+    }
+
+    public override bool TryMakeWorkReservations(bool errorOnFailed) {
+        if (!Unit.Reserve(Job.GetTarget(JobTargetIndex.A),Job))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Gameplay/JobSystem/WorkUtility/WorkUtility_Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class WorkUtility_Food {
+
+    public static Work StartEat()
+    {
+
+    }
+}
